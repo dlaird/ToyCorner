70c8ca9e7170e358046fec4ecd46d06a86bdbb5c
dlaird <dalairds@gmail.com>
2016-10-17 17:06:21 -0500

    Add project files.

---
+++ ToyCorner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToyCorner.Startup))]
+namespace ToyCorner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
